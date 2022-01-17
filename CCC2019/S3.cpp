c9ca2d531b464c59f65f5f9bf624194d7ae4fce4
harshulgupta05 <65627692+harshulgupta05@users.noreply.github.com>
2022-01-16 21:00:52 -0500

    started 2019 S3

---
+++ CCC2019/S3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char grid[3][3];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            char temp;
+
+            cin >> temp;
+
+            grid[i][j] = temp;
+        }
+    }
+
+    // figure out how to test
+
+    return 1;
+}
